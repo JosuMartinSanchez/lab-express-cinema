e3f8344853fa1e1d3e5adc300200d585ee65e00c
Josu Martin <josu.martin.sanchez@gmail.com>
2022-05-08 12:05:42 +0200

    Obligatories

---
+++ models/Movie.model.js
@@ -0,0 +1,27 @@
+
+const {Schema, model}=require("mongoose")
+
+const moviesSchema=new Schema({
+   
+   title:{
+       type:String
+   } ,
+
+   director:{
+       type:String
+   },
+   stars:[{
+    type:String
+   }],
+   image:{
+       type:String
+   },
+   description:{
+       type:String
+   },
+   showtimes:[{
+    type:String
+   }]
+})
+const MoviesModel=model("movie",moviesSchema)
+module.exports=MoviesModel;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,48 @@
 const express = require('express');
+const { route } = require('express/lib/application');
+const async = require('hbs/lib/async');
+const MoviesModel = require('../models/Movie.model');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', async (req,res,next)=>{
+    MoviesModel.find()
+    
+    .then((response) => {
+    
+      console.log(response);
+      res.render("movies.hbs", {
+        
+
+        movieslist: response, 
+      });
+    })
+
+    .catch((err) => {
+      console.log(err);
+    });
+
+})
+
+router.get("/movies/:idmovies", async (req,res,next)=>{
+    const {idmovies} = req.params
+    
+    try{
+        const response = await MoviesModel.findById(idmovies)
+        console.log(response);
+        console.log(req.params);
+
+        res.render('movie-detail.hbs',{
+            details: response
+        })
+
+
+    }catch (err){
+        console.log(err);
+    }
+
+})
 
 module.exports = router;
